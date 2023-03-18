f7f43bdf526c0189f9328d5599f3e1596dd48fe7
Strahinja <strale.alek@gmail.com>
2023-03-18 11:08:50 +0100

    Render the app titles (from data.csv) in two columns

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Strahinja Aleksic")
+    content = """Hi, I am Ardit! I am a Python programmer, teacher, and founder of PythonHow. I graduated in 2013 with a Master of Science in Geospatial Technologies from the University of Muenster in Germany with a focus on using Python for remote sensing.
+    I have worked with companies from various countries, such as the Center for Conservation Geography, to map and understand Australian ecosystems, image processing with the Swiss in-Terra, and performing data mining to gain business insights with the Australian Rapid Intelligence."""
+    st.info(content)
+
+st.write("Below you can find some of the apps I have built in python. Feel free to contact me!")
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.subheader(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.subheader(row["title"])
