0beef5a13214955a96ed87c71498ecedc79339b2
Strahinja <strale.alek@gmail.com>
2023-03-26 00:22:15 +0100

    Secure gmail app password update

--- send_emails.py
+++ send_emails.py
@@ -1,10 +1,12 @@
 import smtplib, ssl
+import os
+
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "aleksic.strahinja06@gmail.com"
-    passwords = "zeupaazpiwbzpxwh"
+    passwords = os.getenv("PORTFOLIOPASSWORD")
 
     context = ssl.create_default_context()
 
